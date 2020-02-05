2bf19d7a2f164560a73e82664f673f022229d119
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-06 00:00:35 +0530

    express uasage

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
-//events are used to control the html browser page(js page).
-//calling multiple events.
-const events=require("events");
-const event=new events.EventEmitter;
-function firstevent(a,b)
-{
-    console.log(a+b);
-    event.emit("click2");//event emited here for sequential purpose.
-}
-function secondevent()
-{
-    console.log("Second event");
-    event.emit("click3");
-}
-function thirdevent()
-{
-    console.log("Third event");
-}
-event.on("clickadd",firstevent);
-event.on("click2",secondevent);
-event.on("click3",thirdevent);
-event.emit("clickadd",4,5);
+const express=require("express");
+const fs=require("fs");
+const data=fs.readFileSync(__dirname+"/hello.txt","utf-8");
+const app=express();
+app.get("/",(req,res)=>{
+   res.send("Hello world");
+})
+app.get("/user",(req,res)=>{
+    res.send("HEllo users");// call it with http://localhost:3000/user
+})
+app.listen(3000);
