81c37f4593c9b04d913d928841f11c07783fb8c9
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-07 00:29:44 +0530

    using the middleware and validation

--- main.js
+++ main.js
@@ -7,11 +7,22 @@ const validation=function(req,res,next)
     console.log("validation running");
     next();
 }
-app.use(validation);
-app.get("/",(req,res)=>{
+const uservalidation=function(req,res,next)
+{
+    if(req.params.userid=="aman")
+    {
+        res.send("The user is validated as aman")
+    }
+    else{
+        res.send("The user is validated not as aman and is some:"+req.params.userid)
+    }
+    next();
+}
+//app.use(validation); will have to use only for global purpose.
+app.get("/",validation,(req,res)=>{
    res.send("Hello world");
 })
-app.get("/user",(req,res)=>{
+app.get("/user/:userid?",uservalidation,(req,res)=>{
     res.send("HEllo users");// call it with http://localhost:3000/user
 })
 app.listen(3000);
