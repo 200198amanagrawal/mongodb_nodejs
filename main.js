bc511d54c8a8fbedf0526a84688d5f9553a16628
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-08 20:29:11 +0530

    validation with express validatior

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const express=require("express");
 var bodyParser = require('body-parser')//defining the bodyparser
+const { check, validationResult } = require('express-validator');
+const urlencoder=bodyParser.urlencoded({extended:false});
 const app=express();
 var jsonParser = bodyParser.json();
-app.use(bodyParser.urlencoded({extended:false}));//mandatory line
+app.use("/abc",express.static("public"));
+app.use(urlencoder);//mandatory line
 app.use(jsonParser);//mandatory line
 app.set("view engine","twig");
 app.set("views","./public/views");
 app.get("/",(req,res)=>{
     res.render("index",{title:"Login page",message:"Hello world"});//simple twig program
  })
-app.post("/",(req,res)=>{
-    res.render("form",{title:"User's page",name:req.body.name,password:req.body.password});
-})
+ app.post("/login",urlencoder,function(req,res){
+    res.send("welcome"+req.body.name);
+ })
+app.post("/",urlencoder,[
+    check("name","Username is invalid").isEmail(),//2nd arg is message for wrong validation.
+    check("password","Password must be more than 5").isLength({min:5})
+]
+,
+(req,res)=>{
+    const errors=validationResult(req);//error is tored here.
+    console.log(errors.mapped());
+    res.render("form",{title:"User's page",error:errors.mapped()});
+});
 app.listen(3000);
