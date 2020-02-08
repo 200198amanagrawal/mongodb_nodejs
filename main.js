bc3f0be883646b0dc7c2ea26354f20b99d0f918c
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-08 11:44:39 +0530

    twig changes with calc

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const app=express();
 app.set("view engine","twig");
 app.set("views","./public/views");
 app.get("/",(req,res)=>{
-    res.render("index",{title:"Twig page",message:"Hello world"});
+    res.render("index",{title:"Twigrs page",message:"Hello world"});//simple twig program
+ })
+ app.get("/about/:a-:b",(req,res)=>{//using some calculation
+     res.render("about",{title:"Calculation",sum:parseInt(req.params.a)+parseInt(req.params.b),sub:req.params.a-req.params.b,
+    mul:req.params.a*req.params.b})
  })
 app.listen(3000);
