f57ea76b45709491415e080b955095e8e8658d4d
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-08 11:04:14 +0530

    pug page

--- main.js
+++ main.js
@@ -1,28 +1,8 @@
 const express=require("express");
-const fs=require("fs");
-const data=fs.readFileSync(__dirname+"/hello.txt","utf-8");
 const app=express();
-const validation=function(req,res,next)
-{
-    console.log("validation running");
-    next();
-}
-const uservalidation=function(req,res,next)
-{
-    if(req.params.userid=="aman")
-    {
-        res.send("The user is validated as aman")
-    }
-    else{
-        res.send("The user is validated not as aman and is some:"+req.params.userid)
-    }
-    next();
-}
-//app.use(validation); will have to use only for global purpose.
-app.get("/",validation,(req,res)=>{
-   res.send("Hello world");
-})
-app.get("/user/:userid?",uservalidation,(req,res)=>{
-    res.send("HEllo users");// call it with http://localhost:3000/user
-})
+app.set("view engine","pug");
+app.set("views","./public/views");
+app.get("/",(req,res)=>{
+    res.render("index",{title:"pug page",message:"Hello world"});
+ })
 app.listen(3000);
