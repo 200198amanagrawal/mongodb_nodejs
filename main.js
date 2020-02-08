760995548e4c1cccd7dce90d4738493bce0850e2
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-08 11:28:19 +0530

    twig changes

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const express=require("express");
 const app=express();
-app.set("view engine","ejs");
+app.set("view engine","twig");
 app.set("views","./public/views");
 app.get("/",(req,res)=>{
-    res.render("index1",{title:"EJS page",message:"Hello world"});
+    res.render("index",{title:"Twig page",message:"Hello world"});
  })
 app.listen(3000);
