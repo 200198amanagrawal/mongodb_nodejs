900b9783f80c76c93a456d1b2d1a5d26393f44a0
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-09 22:00:33 +0530

    sending data to local db

--- mdata.js
+++ mdata.js
@@ -1,4 +1,6 @@
 var mongoose=require("mongoose");//general way to make a schema in mongoose or mongodb.
+mongoose.connect('mongodb://localhost:27017/employee', {useNewUrlParser: true});
+var conn=mongoose.connection;
 var employeeSchema=new mongoose.Schema({
     name:String,
     email:String,
@@ -9,10 +11,23 @@ var employeeSchema=new mongoose.Schema({
 //to define method
 employeeSchema.methods.totalsalary=function()
 {
-        console.log("employees working hour :"+this.hourlyrate*this.totalhous);
+        return this.hourlyrate*this.totalhous;
 }
 var employeeModel=mongoose.model("Employee",employeeSchema);
 var employees=new employeeModel({name:"Aman",email:"amanagrawal20998@gmail.com",
 etype:"hourly",hourlyrate:10,totalhous:16,});
-console.log(employees);
-employees.totalsalary();
+employees.total=employees.totalsalary();
+conn.on("connected",function(){
+    console.log("Connection successfully");
+});
+conn.on("disconnected",function(){
+    console.log("Disconnection successfully");
+});
+conn.on("error",console.error.bind(console,"connection error:"));
+conn.once("open",function(){
+employees.save(function(err,res){
+if(err) throw errors;
+console.log(res);
+conn.close();
+});
+});
